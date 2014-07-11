24e60313a3ec7b79f29933a49bb326f639eb4812
unknown <jpcourt@FRLUMLPT0016840.cpgrp.root.local>
2014-07-11 17:48:30 +0200

    Correction d'une inversion input/output dans le javascript

--- scriptManager.js
+++ scriptManager.js
@@ -1,13 +1,15 @@
 function computeScript(){
-	var scriptText = document.getElementById('scriptOutput').value;
-	var phpContent = '<?php'+scriptText+'?>';
+	var scriptText = document.getElementById('scriptInput').value;
+	var phpContent = '<?php\n'+scriptText+'\n?>';
+
+	console.log('Script saisi :\n'+phpContent);
 
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'editAndExecScript.php?script='+phpContent);
 	xhr.onreadystatechange = function(aEvt) {
 		if (xhr.readyState == 4) {
 			if (xhr.status == 200) {
-				document.getElementById('scriptInput').value = xhr.responseText;
+				document.getElementById('scriptOutput').value = xhr.responseText;
 			}
 		}
 	}
